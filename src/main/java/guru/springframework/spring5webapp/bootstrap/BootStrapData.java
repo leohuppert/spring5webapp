804127a2743acc56b5aed9561ba4a770cfe5cd62
Léo Huppert <leo@huppert.io>
2020-03-31 09:06:40 +0200

    new entity

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,24 +2,35 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class BootstrapData implements CommandLineRunner {
+public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        Publisher publisher = new Publisher();
+        publisher.setName("SFG Publishing");
+        publisher.setCity("St Petersburg");
+        publisher.setState("FL");
+
+        publisherRepository.save(publisher);
+
         Author aut1 = new Author("Auteur", "1");
         Author aut2 = new Author("Auteur", "2");
 
@@ -38,7 +49,15 @@ public class BootstrapData implements CommandLineRunner {
         bookRepository.save(book1);
         bookRepository.save(book2);
 
+        book1.setPublisher(publisher);
+        publisher.getBooks().add(book1);
+        book2.setPublisher(publisher);
+        publisher.getBooks().add(book2);
+
+        publisherRepository.save(publisher);
+
         System.out.printf("Bootstrap");
         System.out.println("Nombres de livres : " + bookRepository.count());
+        System.out.println("Publisher books : " + publisher.getBooks().size());
     }
 }
